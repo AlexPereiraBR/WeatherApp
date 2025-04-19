6b486df7ff9247b6628142ab121f20263b8dafe9
Lyonich <lyonich@icloud.com>
2025-04-20 02:17:14 +0400

    Фикс элементов

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -11,44 +11,38 @@ import SnapKit
 
 class ViewController: UIViewController {
     
+//    1. Это картинка
+//    2. Название города
+//    3. Температура
+//    4. Какие то данные о погоде
+//    5. График
+//    6. Еще какие то данные о погоде
     
+    // 1
+    var weatherImageView = UIImageView()
+    
+    // 2
+    let cityLabel = UILabel()
+    
+    //...
     
     // MARK: - Системные функции
     
-    //Экран БЫЛ загружен
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(named: "Main")
-        setSquare()
-    }
-    
-    //Функия которая создает квадраты view в заданных нами координатах что бы не создавать каждый индивидуально
-    private func setSquare() {
-        let square1 = createSquare(frame: CGRect(x: 34, y: 58, width: 152, height: 28))
-        let square2 = createSquare(frame: CGRect(x: 54, y: 180, width: 304, height: 124))
-        let square3 = createSquare(frame: CGRect(x: 32, y: 380, width: 338, height: 48))
-        let square4 = createSquare(frame: CGRect(x: 32, y: 512, width: 338, height: 218))
+        view.addSubview(weatherImageView)
+        weatherImageView.image = UIImage(named: "тут название изображения")
+        weatherImageView.backgroundColor = . red
         
-        //Затем прогоняем все обекты для инициализации через массив
-        [square1,square2,square3,square4].forEach { item in view.addSubview(item)
+        weatherImageView.snp.makeConstraints { make in
+            make.left.equalTo(50)
+            make.width.height.equalTo(100)
+            make.top.equalTo(100)
         }
         
+        
     }
-    // Функция инициализации квадратов с заданными параметрами
-    private func createSquare(frame: CGRect) -> UIView {
-            let square = UIView()
-            square.backgroundColor = UIColor(named: "Main1")
-            square.frame = frame
-            square.layer.cornerRadius = 20
-            return square
-            }
-        
-
-        
-    
-    //MARK: - Ползовательские функции
-    
     
 }
 
