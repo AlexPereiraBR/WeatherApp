f30afb6a5b4845eb53e5e992c4c088b4d8591523
AlexPereiraBR <aleksandrshchukinbr@icloud.com>
2025-05-05 17:58:29 -0300

    WAPP -13. Рефакторинг кода согласно замечаниям. (Таска на Trello  с описанием)

--- WeatherApp/Constants.swift
+++ WeatherApp/Constants.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+
+struct Constants {
+    static let apiKey = "82b63b257fa6537513b6d200de7e71e4"
+    static let baseURL = "https://api.openweathermap.org/data/2.5/weather"
+    static let savedCityKey = "savedCity"
+    static let viewCitiesKey = "viewCities"
+    static let alertTitleError = "Error"
+    static let alertButtonOK = "OK"
+    static let fallbackWeatherIcon = "cloud.slash"
+    static let language = "en"
+    static let units = "metric"
+}
